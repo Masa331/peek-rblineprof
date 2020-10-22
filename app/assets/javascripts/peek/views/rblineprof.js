dc09a59978a00be4a745ef7ee8763b1c5d9a37df
Premysl Donat <pdonat@seznam.cz>
2020-10-22 09:23:19 +0200

    Add modal toggler

--- app/assets/javascripts/peek/views/rblineprof.js
+++ app/assets/javascripts/peek/views/rblineprof.js
@@ -3,3 +3,14 @@ $(document).on('click', '.js-lineprof-file', function(e) {
   e.preventDefault();
   return false;
 });
+
+$(document).on('click', '.js-lineprof-modal-toggle', function(e) {
+  const toggle = e.target;
+  $('.peek-rblineprof-modal').toggle();
+
+  if (toggle.textContent === 'hide') {
+    toggle.textContent = 'show';
+  } else {
+    toggle.textContent = 'hide';
+  }
+});
